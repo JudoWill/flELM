ea4c1cfc3d464473e9dc1ee27b59aa539373037f
perry <samesense@gmail.com>
2010-03-17 16:00:58 -0400

    changed flu files to flu zip files

--- pavement.py
+++ pavement.py
@@ -4,8 +4,8 @@ from paver.easy import *
 def get_flu_seq():
     """ Grab flu protein fasta & description file from NCBI """
 
-    sh('rsync -av ftp.ncbi.nlm.nih.gov::genomes/INFLUENZA/influenza.faa ./')
-    sh('rsync -av ftp.ncbi.nlm.nih.gov::genomes/INFLUENZA/genomeset.dat ./')
+    sh('rsync -av ftp.ncbi.nlm.nih.gov::genomes/INFLUENZA/influenza.faa.gz ./')
+    sh('rsync -av ftp.ncbi.nlm.nih.gov::genomes/INFLUENZA/genomeset.dat.gz ./')
 
 @task
 def get_host_seq():
