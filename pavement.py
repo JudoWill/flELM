8dc4986016b77258ae7e4f69e58b95453a254b48
Will Dampier <dampier.william@gmail.com>
2010-03-17 17:30:14 -0400

    added the check_elms task to the paver file

--- pavement.py
+++ pavement.py
@@ -1,4 +1,9 @@
 from paver.easy import *
+import os, os.path
+
+import sys
+sys.path.append('.')
+from local_settings import *
 
 @task
 def get_elm_patterns():
@@ -20,3 +25,15 @@ def get_host_seq():
     """ mouse, cow, dog, fish, hourse, chicken, human, rat protein seq from NCBI """
     sh('sh wgetgenome.sh')
 
+
+@task
+def process_elm():
+	"""Determines (and writes) the ELM dictionary"""
+	
+	seq_files = os.listdir(DATADIR)
+	
+	for f in filter(lambda x: x.endswith('.fa'), seq_files):
+		ofile = os.path.join(RESULTSDIR, f.split('.')[0] + '.txt')
+		ifile = os.path.join(DATADIR, f)
+		sh('python makeELMdict.py -o %(out)s %(infile)s' % {'out':ofile, 
+														'infile': ifile})
