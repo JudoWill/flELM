9288148afef0cf4e131d2b767176e6bba887c4d1
Will Dampier <dampier.william@gmail.com>
2010-03-17 20:53:10 -0400

    modified makeELMdict so it will produce fractional counts

--- makeELMdict.py
+++ makeELMdict.py
@@ -43,36 +43,10 @@ def ReadELMs_nocompile(filename):
 	return outdict
 
 
-			
-def ProcessFile(filename):
-	"""Finds the frequencey of ELM instances in a FASTA file.
-	
-	Returns a defaultdict key-ed by ('ELM_NAME', 'ELM_SEQ') and has a value of
-	counts/AA
-	"""
-	
-	elm_dict = ReadELMs('elm_expressions.txt')
-	
-	count_dict = defaultdict(int)
-	
-	len_counter = 0
-	for i, seq in enumerate(SeqGen(filename)):
-		if i % 1000 == 0: print 'processing seq: %d' % i
-		#if i > 1000: break
-		len_counter += len(seq)
-		for elm, reg in elm_dict.items():
-			m = reg.search(seq)
-			while m:
-				count_dict[(elm, m.group())] += 1
-				m = reg.search(seq, m.start()+1)
-	
-	#genome_total = float(len_counter)
-	#for key, val in count_dict.iteritems():
-	#	count_dict[key] = float(val) // genome_total
-	
+		
 	return count_dict
 	
-def ProcessFile_withCloud(filename):
+def ProcessFile(filename):
 	"""Finds the frequencey of ELM instances in a FASTA file.
 
 	Returns a defaultdict key-ed by ('ELM_NAME', 'ELM_SEQ') and has a value of
@@ -108,17 +82,24 @@ def ProcessFile_withCloud(filename):
 	elm_dict = ReadELMs_nocompile('elm_expressions.txt')
 
 	count_dict = defaultdict(int)
-
+	elm_count = defaultdict(int)
+	
 	jids = cloud.map(ProcessSeq, izip(ChunkGen(filename, 100), repeat(elm_dict)))
 	#print str(jids)
 	print 'waiting!'
 	for i, res in enumerate(cloud.iresult(jids)):
 		print 'processing result %d' % i
 		for key, item in res.iteritems():
+			elm, spec = key
 			count_dict[key] += item
-	
-	
-	return count_dict
+			elm_count[elm] += item
+			
+	outdict = {}
+	for key, count in count_dict.iteritems():
+		elm, spec = key
+		outdict[key] = (count, float(count) / float(elm_count[elm]))
+		
+	return outdict
 
 
 
@@ -136,18 +117,21 @@ if __name__ == '__main__':
 	cloud.setkey(CLOUD_KEY, CLOUD_SECRET)
 	if not options.usecloud: cloud.start_simulator()
 	
-	outdata = ProcessFile_withCloud(args[0])
+	outdata = ProcessFile(args[0])
 	print 'writting data'
 	counter = 0
 	with open(options.outfile, 'w') as handle:
 		l = outdata.keys()
 		l.sort()
 		for key in l:
-			val = outdata[key]
+			count, frac = outdata[key]
+			elm, spec = key
+			
+			#print key, val
 			counter += 1
 			#print key[0]
 			#if counter > 100: break
-			handle.write('%s\t%s\t%d\n' % (key[0], key[1], val))
+			handle.write('\t'.join(map(str, [elm, spec, count, '%.10f' % frac]))+'\n')
 	
 	
 	
