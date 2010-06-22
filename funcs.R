bd85d34cb8cb54458bad4e251637560821a82a05
Perry <samesense@gmail.com>
2010-06-22 11:40:19 -0400

    leftovers

--- funcs.R
+++ funcs.R
@@ -1,5 +1,11 @@
-euc_norm<-function(row){row/sqrt(sum(row*row))}
+norm<-function(data){ t(t(data)/rowSums(t(data))) }
 
-euc_norm_rows<-function(mat){t(apply(mat,1,euc_norm))}
+euc_norm<-function(row){ row/sqrt(sum(row*row)) }
 
-cos_dist<-function(dframe){as.dist(crossprod(t(euc_norm_rows(dframe))))}
+euc_norm_rows<-function(mat){ t(apply(mat,1,euc_norm)) }
+
+cos_dist<-function(dframe){ as.dist(acos(crossprod(t(euc_norm_rows(norm(dframe)))))) }
+
+get_monkey<-function(ensembl_ls, outfile){ library('biomaRt'); mart<-useMart('ensembl'); mart<-useMart('ensembl',dataset='mmulatta_gene_ensembl'); seq<-getSequence(id=ensembl_ls, type='ensembl_peptide_id', seqType='peptide', mart=mart); exportFASTA(seq, outfile)}
+
+go_bp_5<-function(ls,outfile){ library('biomaRt'); mart<-useMart('ensembl'); mart<-useMart('ensembl',dataset='hsapiens_gene_ensembl'); bm<-getBM(attributes=c('entrezgene','go_biological_process_id'), filter='entrezgene',values=ls,mart=mart); write.table(bm,file=outfile,sep='\t',append=FALSE,col.names=FALSE,row.names=FALSE,quote=FALSE)}
