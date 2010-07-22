bf368135cb27408b9bd5f6b4b6cecfadb7fc92b8
Perry <samesense@gmail.com>
2010-07-22 08:33:15 -0400

    combined host simple elmdicts

--- combine_elmdicts.py
+++ combine_elmdicts.py
@@ -4,15 +4,22 @@
    results.
 
    Enter results directory & maximum suffix """
-import sys, os, global_settings
+import sys, os, global_settings, utils
+from collections import defaultdict
 
 dir = sys.argv[1] # working/Jul12/
 end = int(sys.argv[2]) # 10
 
-for g in global_settings.TEST_GENOMES:
-    cat_line = ''
+for g in ('H_sapiens', 'Gallus_gallus'):
+    counts = defaultdict(utils.init_zero)
     for x in xrange(end+1):
-        file = dir + 'elmdict_' + g + '.init' + str(x) + ' '
-        cat_line += file
-    os.system('cat ' + cat_line + '> '
-              + dir + 'elmdict_' + g + '.init')
+        file = dir + 'elmdict_' + g + '.simple' + str(x)
+        with open(file) as f:
+            for line in f:
+                str1, str2, count, freq = line.strip().split('\t')
+                counts[str2] += int(count)
+    new_file = dir + 'elmdict_' + g + '.simple'
+    with open(new_file, 'w') as f:
+        for seq in counts:
+            f.write('%s\t%s\t%d\t%s\t\n' %
+                    (seq, seq, counts[seq], '1'))
